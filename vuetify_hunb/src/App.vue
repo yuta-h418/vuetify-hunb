716ab3152168f54da41d48296c38a9798cd91a36
Yuta Hoshina <yuta.h.8888@gmail.com>
2020-06-23 00:23:31 +0900

    ハンバーガーメニュー/モーダル（Veutify）

---
+++ vuetify_hunb/src/App.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey-text text-darken-2">Navigation Lists</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+            <v-list-item-icon>
+              <v-icon>{{ nav_list.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Goggle</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-col class="mb-4">
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">Bookmark</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">User Profile</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field label="Legal first name*" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        label="Legal middle name"
+                        hint="example of helper text only on focus"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        label="Legal last name*"
+                        hint="example of persistent helper text"
+                        persistent-hint
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        label="Legal last name*"
+                        hint="example of persistent helper text"
+                        persistent-hint
+                        required
+                    ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+                <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+
+        <v-btn text>Enterprise</v-btn>
+
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" text>
+              Support
+              <v-icon>mdi-menu-down-outline</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-footer color="primary" dark app>Vuetify</v-footer>
+  </v-app>
+</template>
+
+
+<script>
+
+export default {
+  data: function() {
+    return {
+      drawer: null,
+      items: [
+        { title: "consulting", icon: "mdi-alpha-c-circle-outline" },
+        { title: "management", icon: "mdi-account-group" },
+        { title: "human-resource", icon: "mdi-account-circle" },
+        { title: "company", icon: "mdi-bank-outline" }
+      ],
+      nav_lists: [
+        {
+          name: "Start",
+          icon: "mdi-google"
+        },
+        {
+          name: "Drive",
+          icon: "mdi-folder-google-drive"
+        },
+        {
+          name: "analitics",
+          icon: "mdi-google-analytics"
+        },
+        {
+          name: "map",
+          icon: "mdi-google-earth"
+        },
+        {
+          name: "game",
+          icon: "mdi-google-controller"
+        }
+      ],
+      dialog: false
+    };
+  }
+};
+</script>
